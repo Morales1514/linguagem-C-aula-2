40a8d41b8f5912e87ac0d264740e6b2b29aa18a3
Miguel Morales <117741451+Morales1514@users.noreply.github.com>
2024-09-02 08:07:48 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//este é o primieiro ex do curso de linguagem c 
+int main(void) {
+  //exibe as informações no console 
+  printf("Meu primeiro trabalho em c\n");
+  printf("Meu nome é : Miguel \n");
+  // Sucesso ao cumprir  o codigo 
+  return 0;
+}
